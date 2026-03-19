9cbdd3b1491cf218da14e3d97f9f126cb61870ac
cauetatavittogomes-ops <cauetatavittogomes@gmail.com>
2026-03-19 14:58:10 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,11 @@
+const perfil = document.getElementById("perfil");
+perfil.addEventListener("click", function(){
+    window.open("perfil.html", "_blank");
+
+})
+
+const projetos = document.getElementById("projetos");
+projetos.addEventListener("click", function(){
+    window.open("projetos.html", "_blank");
+})
+
